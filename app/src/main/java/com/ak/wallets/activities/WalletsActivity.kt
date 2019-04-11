7018897414b231325ea03e0eb68e9e45b1d96259
Andrey Kushnaryov <akushnaryov@rightandabove.com>
2019-04-11 18:11:53 +0300

    groupie_recyclerview_wallets

--- app/src/main/java/com/ak/wallets/activities/WalletsActivity.kt
+++ app/src/main/java/com/ak/wallets/activities/WalletsActivity.kt
@@ -2,13 +2,28 @@ package com.ak.wallets.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.ak.wallets.R
+import com.google.firebase.auth.FirebaseAuth
 
 class WalletsActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wallets)
+
+        val uid = FirebaseAuth.getInstance().uid
+        Log.d("WalletActivity"," Working $uid")
+
+        if(uid !== null){
+
+            Toast.makeText(this,"Working...",Toast.LENGTH_LONG).show();
+
+        }
+
 
 
     }
